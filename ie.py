f19517d4e608c0270a7f02527d9a3aa180e57ca6
Andrew Krug <andrew.m.krug@gmail.com>
2017-11-03 12:17:30 -0400

    unsure y these didnt go in

---
+++ ie.py
@@ -0,0 +1,31 @@
+import random
+import threading
+from datetime import datetime
+from functools import wraps
+from threading import Thread
+from time import time
+
+import yaml
+from selenium import webdriver
+from elasticsearch import Elasticsearch
+from selenium.webdriver.common.by import By
+from user_agents import parse
+
+from locator import getting_data
+
+
+def __main__():
+    for _ in range(10000):
+        d = webdriver.Ie()
+        l = getting_data()
+        l.run(d, by=By.XPATH, locator='//table[@id="large-table"]')
+        l.run(d, by=By.PARTIAL_LINK_TEXT, locator='Elemental')
+        l.run(d, by=By.LINK_TEXT, locator='Elemental Selenium')
+        l.run(d, by=By.ID, locator='large-table')
+        l.run(d, by=By.CSS_SELECTOR, locator='#large-table')
+        l.run(d, by=By.CSS_SELECTOR, locator='.column-14')
+        l.run(d, by=By.CLASS_NAME, locator='column-14')
+
+
+if __name__ == '__main__':
+    __main__()
